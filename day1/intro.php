ff932c37e4623a2e24796f9b5ee49eb3cdfea690
lokbhatt <bhattlok117@gmail.com>
2025-05-08 11:02:54 +0545

    This is the first day of my PHP code chalange

---
+++ day1/intro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day 1 PHP session</title>
+</head>
+<style>
+    .box{
+        background-color: aqua;
+        font-family: 'Times New Roman', Times, serif;
+        font-size: 16px;
+        color: black;
+        text-align: justify;
+        width: 800px;
+    }
+</style>
+
+<body>
+    <div class="box">
+        <?php
+        echo "Now starting PHP introduction from beginning.<br> this is my first session sense 2025/5/8";
+        echo "PHP is server scripting language used to create dynamic and interactive web pages.<br>";
+        echo "Like microsoft's ASP widely used and free.<br>";
+        echo "Now break the sentense web page and websites:<br>";
+        echo "Webpage: A single documentation on the internet, typically wtitten in html and accessable via unique URL.<br>
+                Think like sigle page of bbok.<br";
+        echo "Website: is a collection of webpages grouped under single domain name.<br>";
+        echo "Website are two types:<br>
+                1. static: means fixed or doesn't change any content instead developer code manually.<br>
+                2. dynamic: means capable of action or change. content generated in real time and display <br>
+                different content using same sources of code."
+        ?>
+    </div>
+</body>
+
+</html>
