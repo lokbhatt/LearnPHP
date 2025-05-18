9fc1ad095d54666f9e43f2e46736c0c86ac03582
lokbhatt <bhattlok117@gmail.com>
2025-05-18 22:08:00 +0545

    day three introduction is added.

---
+++ day3/introduction.php
@@ -0,0 +1,6 @@
+<?php 
+    echo "Welcome to day 3 session you are now growing please follow the coding tutorials.\n";
+    echo "todays learning lession is string amd array.\n";
+    echo "String is the group of letters and array is block of valiables where all same types are stored.";
+    
+?>
